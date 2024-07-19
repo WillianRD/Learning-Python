b5c84d9f769215a87ffe370920e8c4923f3ec9a0
WillRD <willianrodriguesdos1@gmail.com>
2024-07-18 21:27:54 -0300

    Apredendo a somar notas de alunos de uma Escola

---
+++ MediaAritimética.py
@@ -0,0 +1,4 @@
+n1 = float(input('Primeira nota do Aluno'))
+n2 = float(input('Segubda nota do aluno'))
+final = (n1 + n2) / 2
+print('A média final do aluno é as notas {:.1f} {:.1f} média final {:.1f}'.format(n1,n2,final))
