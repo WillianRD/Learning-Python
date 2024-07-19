713a90698b2980ba32f5660596bb92d7cfdd9057
WillRD <willianrodriguesdos1@gmail.com>
2024-07-18 21:12:46 -0300

    Apredendo sobre como criar um registro de usuário Colocando em prática o que foi passado nas aulas

---
+++ CalcularDoisNumeros.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número'))
+n2 = int(input('Digite um segundo número'))
+valorTotalDaSoma = n1 + n2
+print(f'O valor entre {n1} + {n2} é igual a {valorTotalDaSoma}')
---
+++ Registrando_Usuario.py
@@ -0,0 +1,9 @@
+print('Registar seu cadastro')
+nomeCompleto = input('Digite seu nome completo')
+dataDeNascimento = input('Digite sua data de nascimento')
+nomeDaMae = input('Nome completo da mãe')
+rg = input('Digite seu RG sem traço ou ponto')
+cpf = input('Digite seu CPF sem traço ou ponto')
+print(f'Olá {nomeCompleto} confirma se seus dados estão corretos')
+print('Confirmando dados...'
+      f'\n {nomeCompleto} \n {dataDeNascimento} \n {rg} \n {cpf} \n Muito prazer em te conhecer Senhor (a) {nomeCompleto}')
